34ce5912cec5fdb2e7fbb3615673af0cf1a4e2b3
Helmut Fieres <67054756+HFF-Git@users.noreply.github.com>
2026-03-15 12:39:46 +0100

    Added test code for Attiny1616, started in general library.

---
+++ LcsLib/LcsLib.ino
@@ -0,0 +1,14 @@
+//===========================================================================================
+//
+// This is a dummy file so that we can edit the library files in the Arduino IDE.
+//
+//===========================================================================================
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- Test-1616-I2C-Slave/Test-1616-I2C-Slave.ino
+++ Test-1616-I2C-Slave/Test-1616-I2C-Slave.ino
@@ -3,33 +3,29 @@
 // Install megaTinyCore, use UDE 1.8.13 or 19.
 //
 // Build an adapter: 
-
-// UDPI <---- ---------------< RX
-//           |
-//           --- R --- >| ---< TX          
 //
-// Diode: 1N5819 ( white ring to TX )
-// R:     4K7
+// TX --1k--+
+//         +---- UPDI
+// RX ------+
 //
 // Both serial adapter work. ( serial UDPI, high speed )
 //
 //=========================================================================================== 
 
 //=========================================================================================== 
-// second 214 test.... respond to our I2C address
+// second 1616 test.... respond to our I2C address
 //
-// Uses: 1126 bytes of code, 50 bytes of RAM.
 //=========================================================================================== 
 
 #include <Wire.h>
 
-#define I2C_ADDRESS 0x22      // Just an I2C number
+#define I2C_ADDRESS 0x33      // Just an I2C number
 #define LED_PIN     PIN_PB3
 
 void receiveEvent( int howMany ) {
   
   while ( Wire.available( )) {
-    
+     
     uint8_t cmd = Wire.read( );
   }
 }
